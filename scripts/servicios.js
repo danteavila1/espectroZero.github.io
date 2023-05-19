412c1eea53749073f32ca740a4acdd46d42f1e06
Angela <krewski.angela@gmail.com>
2023-05-19 16:08:37 -0300

    Actualizada lista de servicios

--- scripts/servicios.js
+++ scripts/servicios.js
@@ -14,7 +14,35 @@ let serv =
             "info": "¿Últimamente siente que le están observando? ¿Hay cosas siempre encuentra fuera de lugar? ¿Ve sombras moverse de noche? ¡Es posible que tu hogar se encuentre embrujado! Nuestro equipo puede viajar a cualquier parte del país y eliminar la fuente de sus problemas. (Siempre y cuando sea un fantasma y no un stalker)",
             "precio": 20000,
             "imagen": "logo-simple.png"
-        }
+        },
+        {
+            "id": 2,
+            "nombre": "Edificios modernos y lugares públicos",
+            "info": "Lo crea o no, los fantasmas no solo buscan lugares abandonados, algunos prefieren la compañía de la gente y ver las últimas la televisión, y pueden parecer inocentes, pero son un peligro cuando borran los excels de las rendiciones de cuentas o asustan a tu equipo de futbol local favorito. ¡Nuestro equipo termina con estas amenazas antes del final de temporada!",
+            "precio": 40000,
+            "imagen": "logo-simple.png"
+        },
+        {
+            "id": 3,
+            "nombre": "Hospitales y edificios abandonados",
+            "info": "Ambos tipos de lugares atráen muchas energías negativas y los espectros que los rodean suelen alimentarse de ellas, para eliminarlos, nuestros profesionales podrán capturarlos en contenedores especiales, y además decorar todas las habitaciones con globos y adornos, ¡auyentarán hasta al fantasma más emo que los vea!",
+            "precio": 50000,
+            "imagen": "logo-simple.png"
+        },
+        {
+            "id": 4,
+            "nombre": "Prisiones y estaciones de policía antiguas",
+            "info": "Estos lugares no solo generan muchas energías negativas sino que además los espíritus que habitan sus pasillos suelen ser violentos. ¡Nuestros profesionales para enfrentar estas amenazas necesitan usar su astucia y han entrenado en el jujitsu espiritual y el kickboxing fantasmagórico para asegurarse de no perder en el ring contra ningún maleante! (El precio es más que nada para cubrirles el seguro).",
+            "precio": 70000,
+            "imagen": "logo-simple.png"
+        },
+        {
+            "id": 5,
+            "nombre": "Mansiones abandonadas, cementerios antiguos y altares prohibidos",
+            "info": "Si usted vive en uno de estos lugares, su vida corre mucho peligro, ¡pero no tema! ¡Nuestros profesionales podrán ponerle en contacto en instantes con una inmoviliaria para conseguirle un nuevo lugar donde vivir! Nuestros profesionales se rehusan a ir.",
+            "precio": 5000,
+            "imagen": "logo-simple.png"
+        },
     ]
 };
 
